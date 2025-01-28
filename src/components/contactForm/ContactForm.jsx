26371b2ef60a4e95eb1c7725b407c988ca18c4d2
Olyamh <olyamh234@yahoo.com>
2025-01-28 06:38:38 +0200

    add base form

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,4 +1,31 @@
+import { Field, Form, Formik } from "formik";
+
+
+
 const ContactForm =() =>{
+
+    const initialValues ={
+        name:'',
+    number:''
+    };
+    
+    const handleSubmit = (values, actions) => {
+        actions.resetForm();
+        console.log(values)
+    }
+
+    return (
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+           {({values})=>(
+             <Form >
+             <Field name='name'></Field>
+             <Field name='number' type='number'></Field>
+             <button disabled={!values.name || !values.number} type='submit' >Add contact</button>
+         </Form>
+
+           )}
+        </Formik>
+    )
 
 }
 
