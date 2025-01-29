c9ec9c7c2c57d995560f719f67efd894de1ef75e
Olyamh <olyamh234@yahoo.com>
2025-01-29 09:42:25 +0200

    add form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,51 @@
 import { useState } from 'react';
-import contacts from './contacts.json';
+import initialContacts from './contacts.json';
 import './App.css'
 import ContactForm from './components/contactForm/ContactForm'
 import SearchBox from './components/searchBox/SearchBox'
 import ContactList from './components/contactList/ContactList'
 import Contact from './components/contact/Contact';
+import { nanoid } from 'nanoid/non-secure';
 
 
 const filterContacts = (contacts, filteredValue)=> contacts.filter(contact =>
   contact.name.toLowerCase().includes(filteredValue.toLowerCase().trim())
 );
 function App() {
- const [contact, setContact] = useState(contacts);
+ const [contacts, setContact] = useState(initialContacts);
 
  const [searchValue, setSearchValue] = useState('');
- const filteredContacts =filterContacts (contact, searchValue)
+ const filteredContacts =filterContacts (contacts, searchValue);
+
+ const addContacts = newContact =>{
+  const checkName = contacts.find(contact => contact.name.toLocaleLowerCase() === newContact.name.toLocaleLowerCase().trim());
+  const checkNumber = contacts.find(contact => contact.number === newContact.number);
+
+  if(checkName){
+   alert('this name is in your phone book');
+   return;
+  }
+  if (checkNumber){
+    alert('this number is in your phone book');
+    return;
+  }
+  newContact.id=nanoid();
+  setContact(prev => [...prev, newContact])
+  
+ }
 
 const handleDelete = id =>{
-  const newContactList = contact.filter(item => item.id !== id);
+  const newContactList = contacts.filter(item => item.id !== id);
   setContact(newContactList);
 };
 const handleSearchChange = e =>{
   console.log(e.target.value)
   setSearchValue(e.target.value)
 }
-
-
-// const searchContacts = contacts.filter(contact => contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase().trim()))
-
-
-
   return (
     <div>
   <h1>Phonebook</h1>
-  <ContactForm />
+  <ContactForm addContacts={addContacts}/>
   <SearchBox value={searchValue} onChange={handleSearchChange}/>
   <ContactList contacts={filteredContacts} handleDelete={handleDelete}/>
 </div>
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,32 +1,82 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
+const ContactForm = ({addContacts}) => {
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+//   const phoneRegex = /^(\d{3}-\d{2}-\d{2}|\d{7})$/;
+//   const nameRegex = /^[A-Za-zА-Яа-яЄєІіЇїҐґ-\s]+$/;
 
+//   const ContactSchema = Yup.object().shape({
+//     name: Yup.string()
+//       .min(3, 'The name must contain more than 3 characters')
+//       .max(50, 'The name must contain up to 50 characters')
+//       .required('This field is required')
+//       .matches(nameRegex, 'Enter only letters'),
+//     number: Yup.string()
+//       .required('This field is required')
+//       .min(7, 'The number must contain at least 7 characters')
+//       // .max(9, 'The number must contain up to 9 characters')
+//       .matches(phoneRegex, 'Phone number format ХХХ-ХХ-ХХ'),
+//   });
 
-const ContactForm =() =>{
+  const nameRegex = /^[A-Za-zА-Яа-яЄєІіЇїҐґ-\s]+$/;
+  const phoneNumberRegex = /^\+?3?8?(0\d{9})$/;
 
-    const initialValues ={
-        name:'',
-    number:''
-    };
-    
-    const handleSubmit = (values, actions) => {
-        actions.resetForm();
-        console.log(values)
-    }
+  const formSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "The name must contain more than 2 characters")
+      .max(50, "The name must contain up to 50 characters")
+      .required("This field is required")
+      .matches(nameRegex, "Enter only letters"),
+    // number: Yup.string()
+    //   .min(7, "The number must contain at least 7 characters")
+    //   .max(10, "The number must contain up to 10 characters")
+    //   .matches(
+    //     phoneNumberRegex,
+    //     "Please enter your phone number in the following format: XXX-XX-XX"
+    //   )
+    //   .required("This field is required"),
+  });
 
-    return (
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-           {({values})=>(
-             <Form >
-             <Field name='name'></Field>
-             <Field name='number' type='number'></Field>
-             <button disabled={!values.name || !values.number} type='submit' >Add contact</button>
-         </Form>
+  const handleSubmit = (values, actions) => {
+    actions.resetForm();
+    addContacts(values);
+  };
 
-           )}
-        </Formik>
-    )
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={formSchema}
+    >
+      {({ values }) => (
+        <Form>
+          <div>
+            <label>
+              Name
+              <Field name="name" />
+              <ErrorMessage name="name" component="span"></ErrorMessage>
+            </label>
+          </div>
 
-}
+          <div>
+            <label>
+              Phone
+              <Field name="number" />
+              <ErrorMessage name="number" component="span" />
+            </label>
+          </div>
+
+          <button disabled={!values.name || !values.number} type="submit">
+            Add contact
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default ContactForm;
